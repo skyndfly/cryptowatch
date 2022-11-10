03fcdd9864d522508c9cf4c7db0a7ac5ebfb8bfe
Sergey Golubev <slyandy2796@yandex.ru>
2022-11-10 16:59:35 +0300

    Добавление и удаление тикеров

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
-<template>Начало работы</template>
+<template>
+  <div class="add_wrapper">
+    <p>Тикер:</p>
+    <input v-model="ticker" @keydown.enter="add" type="text" />
+    <button @click="add">Добавить</button>
+  </div>
+
+  <template v-if="tickers.length">
+    <div class="added_wrapper">
+      <div v-for="t in tickers" :key="t.name" class="item">
+        <p class="name">{{ t.name }}</p>
+        <h2 class="price">{{ t.price }}</h2>
+        <button class="delete" @click="deleteTicker(t)">Удалить</button>
+      </div>
+    </div>
+  </template>
+</template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      ticker: null,
+      tickers: [],
+    };
+  },
+  methods: {
+    add() {
+      this.tickers.push({ name: this.ticker, price: 11 });
+      this.ticker = "";
+    },
+    deleteTicker(ticker) {
+      this.tickers = this.tickers.filter((t) => t !== ticker);
+    },
+  },
 };
 </script>
 
-<style></style>
+<style src="./app.css"></style>

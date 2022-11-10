09405375202c4f8e20b241ceda2551400917ba21
Sergey Golubev <slyandy2796@yandex.ru>
2022-11-10 18:53:11 +0300

    подключил api

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,26 @@
 
   <template v-if="tickers.length">
     <div class="added_wrapper">
-      <div v-for="t in tickers" :key="t.name" class="item">
+      <div
+        v-for="t in tickers"
+        :key="t.name"
+        :class="{ active: active === t }"
+        class="item"
+        @click="active = t"
+      >
         <p class="name">{{ t.name }}</p>
         <h2 class="price">{{ t.price }}</h2>
-        <button class="delete" @click="deleteTicker(t)">Удалить</button>
+        <button class="delete" @click.stop="deleteTicker(t)">Удалить</button>
       </div>
     </div>
   </template>
+  <template v-if="active">
+    <div class="graph">
+      <button type="button" @click="handleDisabledGraph">X</button>
+      <h2>{{ active.name }}</h2>
+    </div>
+  </template>
 </template>
-
 <script>
 export default {
   name: "App",
@@ -23,15 +34,28 @@ export default {
     return {
       ticker: null,
       tickers: [],
+      active: null,
     };
   },
   methods: {
     add() {
-      this.tickers.push({ name: this.ticker, price: 11 });
+      const newTicker = { name: this.ticker, price: 11 };
+      setInterval(async () => {
+        const api = await fetch(
+          `https://min-api.cryptocompare.com/data/price?fsym=${newTicker.name}&tsyms=usd`
+        );
+        const data = await api.json();
+        console.log(data);
+      }, 3000);
+      this.tickers.push(newTicker);
       this.ticker = "";
     },
     deleteTicker(ticker) {
+      this.active = null;
       this.tickers = this.tickers.filter((t) => t !== ticker);
+    },
+    handleDisabledGraph() {
+      this.active = null;
     },
   },
 };
